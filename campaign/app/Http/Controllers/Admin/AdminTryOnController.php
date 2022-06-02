ac8f896b8ebe61860fbe150cfc9d3daf6b84a7d0
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2022-06-02 23:34:33 +0900

    ページネーション修正

--- campaign/app/Http/Controllers/Admin/AdminTryOnController.php
+++ campaign/app/Http/Controllers/Admin/AdminTryOnController.php
@@ -20,7 +20,7 @@ class AdminTryOnController extends BaseController
     public function index()
     {
         //
-        $tryOns = TryOn::paginate(5);
+        $tryOns = TryOn::paginate(20);
         return view('admin.try_on.index', compact('tryOns'));
     }
 
