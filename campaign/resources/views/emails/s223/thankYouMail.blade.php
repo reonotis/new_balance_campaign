cfc34becaa31730b129c39f2eac1a811bf4ef57a
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2023-07-31 09:32:34 +0900

    メール文面修正

--- campaign/resources/views/emails/s223/thankYouMail.blade.php
+++ campaign/resources/views/emails/s223/thankYouMail.blade.php
@@ -3,7 +3,7 @@
 この度はmyNB会員様限定、<br>
 New Balance Fall ＆ Winter 2023 Apparel Collection (8月10日開催) へのご応募ありがとうございます。<br>
 <br>
-当選者の発表は8月6日（木）以降に、当選者様への直接のご連絡をもってかえさせていただきます。<br>
+当選者の発表は8月6日（木）-8月7日（金）に、当選者様への直接のご連絡をもってかえさせていただきます。<br>
 引き続き、ニューバランスをよろしくお願いいたします。<br>
 <br>
 New Balance Fall ＆ Winter 2023 Apparel Collection事務局<br>
