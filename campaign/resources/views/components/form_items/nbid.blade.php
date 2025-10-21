b8a49d65e33e304744a9837d3c2daf89cd6c1861
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2025-10-21 11:38:18 +0900

    NBIDの注釈分修正

--- campaign/resources/views/components/form_items/nbid.blade.php
+++ campaign/resources/views/components/form_items/nbid.blade.php
@@ -7,7 +7,9 @@
         <div class="flex w-72 px-2">
             <input type="text" name="nbid" id="nbid" value="{{ old("nbid") }}" class="form-control">
         </div>
-        <p class="mail_supplement mb-1">※会員登録がお済の場合はご入力ください。</p>
+        <p class="mail_supplement mb-1">※イベントの申し込みにはニューバランスmyNB会員への登録が必須です。</p>
+        <p class="mail_supplement mb-1">※登録がお済みでない場合、下記より新規会員登録をしてからお申し込みください。</p>
+        <p class="mail_supplement mb-1"><a href="https://shop.newbalance.jp/login" target="_blank">https://shop.newbalance.jp/login</a></p>
         <p class="mail_supplement mb-1">※myNBIDについては下記よりご確認ください。<br><a href="https://shop.newbalance.jp/guide-faq-mynb.html" target="_blank">https://shop.newbalance.jp/guide-faq-mynb.html</a></p>
     </div>
 </div>
