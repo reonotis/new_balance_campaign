ddecde826487aae14f3d96adad8429399e1333de
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2025-10-24 21:51:15 +0900

    句点を追加

--- campaign/resources/views/components/form_items/email.blade.php
+++ campaign/resources/views/components/form_items/email.blade.php
@@ -3,8 +3,8 @@
 <div class="item-row">
     <label for="email" class="item-title">メールアドレス</label>
     <div class="item-content px-2">
-        <p class="mail_supplement mb-1">※myNB会員ご登録の方は、会員登録しているメールアドレスをご記載ください</p>
-        <p class="mail_supplement mb-1">※メールアドレスは確認用の為、2回入力してください</p>
+        <p class="mail_supplement mb-1">※myNB会員ご登録の方は、会員登録しているメールアドレスをご記載ください。</p>
+        <p class="mail_supplement mb-1">※メールアドレスは確認用の為、2回入力してください。</p>
         <p class="mail_supplement mb-1">
             ※docomo、au、softbank、iCloud等の各キャリアのメールアドレスは、ドメイン指定受信を設定されている可能性がありメールが正しく届かない事がある為、PC用のメールアドレスを記入する事をお勧め致します。</p>
         <div class="">
